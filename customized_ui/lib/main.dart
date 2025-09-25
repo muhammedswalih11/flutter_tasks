7b68193e828caf7056fcea5a39fc652fff3dddac
muhammedswalih11 <swalih.muhammed@omnicoredigital.com>
2025-09-25 17:58:25 +0530

    25/09/25

---
+++ customized_ui/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:customized_ui/features/presentation/pages/home.dart';
+import 'package:customized_ui/features/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: Home(),
+        );
+      },
+    );
+  }
+}
